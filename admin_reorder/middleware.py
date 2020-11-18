5dda7bdfd9e54de7f6c865af0c99a59566dde264
Bradley D. Peters <bdp@moberley.com>
2020-11-17 20:06:05 -0600

    Added support for Django 3.1 admin sidebar (merged with admin_site support)

--- admin_reorder/middleware.py
+++ admin_reorder/middleware.py
@@ -171,12 +171,16 @@ class ModelAdminReorderMiddleware:
             # or app_list view, bail out!
             return response
 
-        try:
+        if 'app_list' in response.context_data:
             self.app_list = response.context_data['app_list']
-        except KeyError:
-            # there is no app_list! nothing to reorder
+            context_key = 'app_list'
+        # handle django 3.1 sidebar
+        elif 'available_apps' in response.context_data:
+            self.app_list = response.context_data['available_apps']
+            context_key = 'available_apps'
+        else:  # nothing to reorder, return response
             return response
 
-        ordered_app_list = self.get_app_list(self.admin_site_name)
-        response.context_data['app_list'] = ordered_app_list
+        ordered_app_list = self.get_app_list()
+        response.context_data[context_key] = ordered_app_list
         return response
